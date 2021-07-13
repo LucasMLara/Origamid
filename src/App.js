ba6573fa98934009253f3884df3b1a439aa40ce8
LucasMunizLara <lucasml@live.com>
2021-07-13 15:48:10 -0300

    starting exercise

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-const mario = {
-  cliente: "Mario",
-  idade: 31,
-  compras: [
-    {nome: 'Notebook', preco: 'R$ 2500'},
-    {nome: 'Geladeira ', preco: 'R$ 3000'},
-    {nome: 'Smartphone', preco: 'R$ 1500'},
-    {nome: 'Guitarra', preco: 'R$ 3500'},
-  ],
-  ativa: false,
-};
-
-const luana = {
-  cliente: "Luana",
-  idade: 27,
-  compras: [
-    {nome: 'Notebook', preco: 'R$ 2500'},
-    {nome: 'Geladeira ', preco: 'R$ 3000'},
-    {nome: 'Smartphone', preco: 'R$ 1500'},
-  ],
-  ativa: true,
-};
+const produtos = [
+  {
+    id:1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id:2,
+    nome: 'NoteBook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id:3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+]
 
 function App() {
-  const dados = luana
-  return <span>Exercícios de Fixação</span>;
+  
+    return <ul>Exercícios de Fixação</ul>;
 }
 
 export default App;
