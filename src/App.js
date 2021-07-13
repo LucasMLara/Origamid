29daa1e9fc8360eb74675f79c7a5c139e9e88611
LucasMunizLara <lucasml@live.com>
2021-07-13 12:09:04 -0300

    Início do exercício

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    {nome: 'Notebook', preco: 'R$ 2500'},
+    {nome: 'Geladeira ', preco: 'R$ 3000'},
+    {nome: 'Smartphone', preco: 'R$ 1500'},
+    {nome: 'Guitarra', preco: 'R$ 3500'},
+  ],
+  ativa: false,
+};
+
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    {nome: 'Notebook', preco: 'R$ 2500'},
+    {nome: 'Geladeira ', preco: 'R$ 3000'},
+    {nome: 'Smartphone', preco: 'R$ 1500'},
+  ],
+  ativa: true,
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const dados = luana
+  return <span>Exercícios de Fixação</span>;
 }
 
 export default App;
